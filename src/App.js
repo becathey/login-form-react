d21d2a4ebe8524ad2fbc1b82738319042d276f96
Bill Cathey <billcathey@hotmail.com>
2021-11-11 22:47:38 -0500

    Create form, add styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="container">
+      <form>
+        <h1>Registration Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
