755e68ec29224ac1ded9fc23570d31396fc261ab
Bill Cathey <billcathey@hotmail.com>
2021-11-12 13:59:18 -0500

    Create state, bind form values to state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const initialValues = { username: "", email: "", password: "" };
+  const { formValues, setFormValues } = useState(initialValues);
   return (
     <div className="container">
       <form>
@@ -9,15 +12,30 @@ function App() {
         <div className="ui form">
           <div className="field">
             <label>Username</label>
-            <input type="text" name="username" placeholder="Username" />
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formValues.username}
+            />
           </div>
           <div className="field">
             <label>Email</label>
-            <input type="email" name="email" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+            />
           </div>
           <div className="field">
             <label>Password</label>
-            <input type="password" name="password" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formValues.password}
+            />
           </div>
           <button className="fluid ui button blue">Submit</button>
         </div>
