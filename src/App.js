aec59adca87eff5b7810d7fb6e7559e0f119b8ec
Bill Cathey <billcathey@hotmail.com>
2021-11-14 16:52:39 -0500

    Add email and password format validation

--- src/App.js
+++ src/App.js
@@ -31,9 +31,15 @@ function App() {
     }
     if (!values.email) {
       errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
     }
     if (!values.password) {
       errors.password = "Password is required!";
+    } else if (values.password.length < 6) {
+      errors.password = "Password must be 6 or more characters!";
+    } else if (values.password.length > 12) {
+      errors.password = "Password cannot exceed 12 characters!";
     }
     return errors;
   };
